8da4362658cb35a673fae5615b6ed878d3c38e3d
Tim Regan-Porter <timreganporter@gmail.com>
2020-09-09 16:56:31 -0400

    Refactor to add a reusable reducer pattern

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
       <Text>IndexScreen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
