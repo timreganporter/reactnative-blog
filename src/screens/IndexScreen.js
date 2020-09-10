f2d92ce64f47b81b47bb821fff1cda07522de620
Tim Regan-Porter <timreganporter@gmail.com>
2020-09-09 20:02:23 -0400

    Add a little styling to list

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 
@@ -8,19 +9,38 @@ const IndexScreen = () => {
 
   return (
     <View>
-      <Text>IndexScreen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name="trash" style={styles.icon}/>
+            </View>
+          );
         }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    borderColor: 'gray',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 20
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
